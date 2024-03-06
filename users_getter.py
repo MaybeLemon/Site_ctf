533f3acc015af41961eb9b0c2d8ad91571ac7e80
MaybeLemon <delemon31@mail.ru>
2024-03-06 22:02:16 +0300

    .

---
+++ users_getter.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+def users_getter():
+    if os.path.exists('users.json'):
+        with open('users.json', 'r', encoding='utf-8') as json_file:
+             return json.load(json_file)
+    else:
+        return {}
+
+def save_users(users):
+    with open('users.json', 'w', encoding='utf-8') as json_file:
+        json.dump(users, json_file, ensure_ascii=False, indent=4)
